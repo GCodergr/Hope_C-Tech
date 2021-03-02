5f803567507fd4ceb0b307e25c2ed3ab2e8d400a
GCodergr <dimitris_gd@hotmail.com>
2021-03-02 19:03:29 +0200

    Added texture rendering

--- Hope C-Tech/assets/shaders/3.3.shader.frag
+++ Hope C-Tech/assets/shaders/3.3.shader.frag
@@ -1,8 +1,20 @@
 #version 330 core
 
-out vec4 FragColor;
 in vec3 ourColor;
+in vec2 TexCoord;
+
+out vec4 FragColor;
+
+// texture samplers
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main(){
-    FragColor = vec4(ourColor, 1.0f);    
+    // Output texture color
+    //FragColor = texture(ourTexture, TexCoord);
+    // Mix vertex's color and the texture's color 
+    //FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0); 
+
+    // Linearly interpolate between both textures (80% bricks, 20% duck)
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
--- Hope C-Tech/src/Hope C-Tech.cpp
+++ Hope C-Tech/src/Hope C-Tech.cpp
@@ -4,6 +4,7 @@
 #include "Shader.h"
 
 #include "SDL.h"
+#include "utilities/stb_image.h"
 
 typedef uint32_t u32;
 typedef int32_t b32;
@@ -46,31 +47,104 @@ int main(int argv, char** args) {
 
 	// Build and compile shader program
 	Shader ourShader(".\\assets\\shaders\\3.3.shader.vert", ".\\assets\\shaders\\3.3.shader.frag");
-	
+
+	// Set up vertex data (and buffer(s)) and configure vertex attributes
 	float vertices[] = {
-			 // Positions			// colors			 
-			 0.5f,  -0.5f,  0.0f,	1.0f, 0.0f, 0.0f,	// bottom right
-			-0.5f,  -0.5f,  0.0f,	0.0f, 1.0f, 0.0f,   // bottom left
-			 0.0f,   0.5f,  0.0f,	0.0f, 0.0f, 1.0f	// top
+			 // Positions			// colors			// texture coordinates  
+		     0.5f,   0.5f,  0.0f,   1.0f, 0.0f, 0.0f,	1.0f,  1.0f,      // top right 
+			 0.5f,  -0.5f,  0.0f,	0.0f, 1.0f, 0.0f,	1.0f,  0.0f,      // bottom right 
+			-0.5f,  -0.5f,  0.0f,	0.0f, 0.0f, 1.0f,   0.0f,  0.0f,      // bottom left 
+			-0.5f,   0.5f,  0.0f,	1.0f, 1.0f, 0.0f,	0.0f,  1.0f       // top left
+	};
+	unsigned int indices[] = {
+		0, 1, 3,	// first triangle
+		1, 2, 3	// second triangle
 	};
 	
-	GLuint VBO, VAO; // 
+	GLuint VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);	
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	// 1. Bind the Vertex Array Object
 	glBindVertexArray(VAO);
 	// 2. Bind and set vertex buffer(s)
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	
 	// 3. Configure vertex attributes
 	// position attribute 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-		
+	// texture coordinate attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	// Load and create a textures 
+	//
+	// Texture 1
+	//
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	// Set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// Set the texture filtering parameters 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Load image, create texture and generate mipmaps 
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+	// Load image, create texture and generate mipmaps
+	unsigned char* data = stbi_load(".\\assets\\textures\\Bricks042_1K_Color.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Error::Failed to load Brick texture" << std::endl;
+	}
+	stbi_image_free(data); 
+
+	//
+    // Texture 2
+    //
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	// Set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// Set the texture filtering parameters 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Load image, create texture and generate mipmaps
+	data = stbi_load(".\\assets\\textures\\duck.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Error::Failed to load Duck texture" << std::endl;
+	}	
+	stbi_image_free(data);
+
+	// Tell OpenGL for each sampler to which texture unit it belongs to (only has to be done once)
+	ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
+	// either set it manually like so...
+	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+	// ...or set it via the texture class
+	ourShader.setInt("texture2", 1);
 	
 	b32 Running = 1;
 	b32 FullScreen = 0;
@@ -114,18 +188,25 @@ int main(int argv, char** args) {
 
 		// Note: Uncomment for wire-frame rendering
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	    // bind textures on corresponding texture units
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 		
-		// render the triangle
+		// Render container
 		ourShader.use();		
-		glBindVertexArray(VAO);		
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-			
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+				
 		SDL_GL_SwapWindow(window);
 	}
 
 	// optional: de-allocate all resources once they've outlived their purpose:
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	
 	return 0;
 }
